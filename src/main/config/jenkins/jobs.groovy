94cbb1c054c88ad6986825022ae43d3cc23febb2
Emmanuel Vasseur <vasseur.emmanuel@gmail.com>
2016-11-24 08:31:57 +0100

    Replace the two regular team jobs by a pipeline job, and remove the env var LOCAL_SEED

--- src/main/config/jenkins/jobs.groovy
+++ src/main/config/jenkins/jobs.groovy
@@ -1,90 +1,91 @@
 job('Seed-configuration') {
     scm {
-        git("true".equalsIgnoreCase(System.getenv('LOCAL_SEED')) ?
-                'C:/Applications/Workspace/score-exercise-example-validation' :
-                'https://github.com/emmanuel-vasseur/score-exercise-example-validation.git')
+        git 'D:/dev/workspace-neon/score-exercise-example-validation'
     }
     triggers {
-        cron('H * * * *')
-        scm('H/5 * * * *')
+        cron '0 * * * *'
+        scm 'H/15 * * * *'
     }
     steps {
 		jobDsl {
-			targets('src/main/config/jenkins/jobs.groovy')
-			removedJobAction('DELETE')
-			removedViewAction('DELETE')
-			failOnMissingPlugin(true)
-			unstableOnDeprecation(true)
+			targets 'src/main/config/jenkins/jobs.groovy'
+			removedJobAction 'DELETE'
+			removedViewAction 'DELETE'
+			failOnMissingPlugin true
+			unstableOnDeprecation true
 		}
 	}
 }
 
 job('Build-battlecode-framework') {
     scm {
-        git("true".equalsIgnoreCase(System.getenv('LOCAL_SEED')) ?
-                'C:/Applications/Workspace/score-project' :
-                'https://github.com/2nis6mon/score-project.git')
+        git 'https://github.com/2nis6mon/score-project.git'
     }
     triggers {
-        scm('H/5 * * * *')
+        scm 'H/5 * * * *'
     }
     steps {
-        maven('clean install')
+        maven 'clean install'
     }
 }
 
 job('Build-battlecode-exercise') {
     scm {
-        git("true".equalsIgnoreCase(System.getenv('LOCAL_SEED')) ?
-                'C:/Applications/Workspace/score-exercise-example' :
-                'https://github.com/emmanuel-vasseur/score-exercise-example.git')
+        git 'https://github.com/emmanuel-vasseur/score-exercise-example.git'
     }
     triggers {
-        scm('H/5 * * * *')
+        scm 'H/5 * * * *'
     }
     steps {
-        maven('clean install')
+        maven 'clean install'
     }
 }
 
-hudson.model.User.all.findAll{ user ->
-	user.id != 'admin' && user.getProperty(hudson.security.HudsonPrivateSecurityRealm.Details) != null
-}.each { user ->
-	job("Build-battlecode-${user.id}-implementation") {
-		scm {
-			git("true".equalsIgnoreCase(System.getenv('LOCAL_SEED')) ?
-					"C:/Applications/Workspace/score-exercise-${user.id}-impl" :
-					"https://github.com/emmanuel-vasseur/score-exercise-${user.id}-impl.git")
-		}
-		steps {
-			maven {
-				goals('clean verify')
-				properties('maven.test.skip': true, 'jar.finalName': "battlecode-2016-${user.id}-impl")
-			}
-			maven {
-				goals('install:install-file')
-				properties(file: "target/battlecode-2016-${user.id}-impl.jar", groupId: 'org.vasseur', artifactId: "battlecode-2016-${user.id}-impl", version: '0.1-SNAPSHOT', pomFile: 'pom.xml')
-			}
+hudson.model.User.all.findAll{ team ->
+	team.id != 'admin' && team.getProperty(hudson.security.HudsonPrivateSecurityRealm.Details) != null
+}.each { team ->
+	pipelineJob("${team.id}-Battlecode-Build") {
+		concurrentBuild false
+		definition {
+			cps {
+				sandbox true
+				script """\
+node {
+	stage('Clone ${team.id} repository') {
+		git 'https://github.com/emmanuel-vasseur/score-exercise-${team.id}-impl.git'
+	}
+	stage('Build implementation') {
+		maven 'clean verify', ['maven.test.skip': true, 'jar.finalName': 'battlecode-2016-${team.id}-impl']
+	}
+	stage('Publish implementation') {
+		maven 'install:install-file', [file: 'target/battlecode-2016-${team.id}-impl.jar',
+				groupId: 'org.vasseur', artifactId: 'battlecode-2016-${team.id}-impl',
+				version: '0.1-SNAPSHOT', pomFile: 'pom.xml']
+	}
+	stage('Clone validation repository') {
+		deleteDir()
+		git 'https://github.com/emmanuel-vasseur/score-exercise-example-validation.git'
+	}
+	stage('Validation implementation') {
+		timeout(2) {
+			maven 'clean test', ['maven.test.failure.ignore': true, team: '${team.id}']
 		}
 	}
-
-	job("Build-battlecode-${user.id}-validation") {
-		scm {
-			git("true".equalsIgnoreCase(System.getenv('LOCAL_SEED')) ?
-					'C:/Applications/Workspace/score-exercise-example-validation' :
-					'https://github.com/emmanuel-vasseur/score-exercise-example-validation.git')
-		}
-		wrappers {
-			timeout {
-				absolute(2)
-				failBuild()
-				writeDescription('Build failed due to a long/stuck build time')
-			}
-		}
-		steps {
-			maven {
-				goals('clean test')
-				properties('maven.test.failure.ignore': true, 'team': user.id)
+}
+""" + '''\
+def Closure maven(task, properties) {
+	def mavenCommandLine = "mvn -B $task ${joinProperties(properties)}"
+	if(isUnix()) {
+		sh mavenCommandLine
+	} else {
+		bat mavenCommandLine
+	}
+}
+@NonCPS
+def joinProperties(properties) {
+    properties.collect{ key, value -> "\\\"-D$key=$value\\\"" }.join(' ')
+}
+'''
 			}
 		}
 	}
